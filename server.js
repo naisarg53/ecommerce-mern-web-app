c8f47117010710b48c1adc443068af6359a93596
naisarg53 <59700346+naisarg53@users.noreply.github.com>
2020-09-10 14:58:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -40,10 +40,6 @@ app.use('/payment', Payments);
 if (process.env.NODE_ENV === 'production') {
 
     app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
 }
 
 app.listen(port, function () {
